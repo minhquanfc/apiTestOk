4ed102af5506affd87b0a49ffe33527bc684ada1
minhquanfc <quanminhfc@gmail.com>
2023-11-01 21:19:30 +0700

    update

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,6 +2,8 @@ const ytch = require('yt-channel-info');
 const {channelId} = require("./GetYouTubeIdByUrl");
 const {google} = require('googleapis');
 const youtube = google.youtube('v3');
+const axios = require('axios');
+const {token} = require("morgan");
 
 exports.postApi = async (req, res, next) => {
     try {
@@ -55,4 +57,40 @@ const getCountSub = async (id) => {
     } catch (error) {
         throw new Error("Error in getCountSub: " + error);
     }
+}
+
+exports.callApi = (req,res,next)=>{
+    const token = "Bearer 590|Ynmw6oAweZBdpyIPOBhL8kgO9XwTPX3niIKEhSHh"
+    axios.get('http://subcmc.api-simplesolution.one/api/v2/order/history/list?fromDay=10-09-2023&toDay=31-10-2023&user_id=-1&cancel=0&service_id=0',{
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(async response => {
+            // console.log(response.data.data);
+            const data123 = response.data.data;
+            for (let i = 0; i < data123.length; i++) {
+                // console.log(data123[i].order_id)
+                await callApicheck(data123[i].order_id, token);
+            }
+            res.render('index', {title: 'Express'});
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+// http://subcmc.api-simplesolution.one/api/v2/order/history/list?fromDay=07-10-2023&toDay=14-10-2023&user_id=-1&cancel=1&service_id=0
+// callApicheck("267644","Bearer 590|Ynmw6oAweZBdpyIPOBhL8kgO9XwTPX3niIKEhSHh")
+async function callApicheck(id,token) {
+    axios.get('http://subcmc.api-simplesolution.one/api/v2/order/restore/'+id, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            console.log(response.data.data.message);
+        })
+        .catch(error => {
+            console.error("Loi "+id);
+        });
 }
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 const apiC = require('../controllers/api.controller');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', apiC.callApi);
 router.post('/api/remains', apiC.postApi);
 
 module.exports = router;
